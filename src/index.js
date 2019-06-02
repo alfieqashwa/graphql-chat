a3a448da0d71cf88697e1981af777f43c455993d
alfieqashwa <alfieqashwa@gmail.com>
2019-06-03 01:46:40 +0700

    First Commit chptr-4

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import { ApolloServer } from 'apollo-server-express';
+import express from 'express';
+import resolvers from './resolvers';
+import typeDefs from './typeDefs';
+
+const { APP_PORT = 4000, NODE_ENV = 'development' } = process.env;
+const IN_PROD = NODE_ENV === 'production';
+
+const app = express();
+
+app.disable('x-powered-by');
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  playground: !IN_PROD
+});
+
+server.applyMiddleware({ app });
+
+app.listen({ port: APP_PORT }, () =>
+  console.log(`http://localhost:${APP_PORT}${server.graphqlPath}`)
+);
